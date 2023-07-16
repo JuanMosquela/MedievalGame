52a07b41ead98e9d7c9ccd649009a111c1f89361
JuanMosquella <94761627+JuanMosquela@users.noreply.github.com>
2023-07-16 02:47:01 -0300

    hover button effect

--- button.py
+++ button.py
@@ -8,6 +8,7 @@ class Button:
         self.font = pygame.font.SysFont("Arial", 50)
         self.border_color = border_color
         self.text = text
+        self.color = color
         self.text_surface = self.font.render(
             self.text, True, (255, 255, 255))
         self.rect = self.text_surface.get_rect()
@@ -15,18 +16,36 @@ class Button:
         self.button_height = self.rect.height
         self.rect.x = (screen_width - self.button_width) // 2
         self.rect.y = ((screen_height - self.button_height) // 2) + offset
-        self.hover = False
+        self.is_hover = False
 
     def check_clicked(self, mouse_pos):
         if self.rect.collidepoint(mouse_pos):
             return True
         else:
             return False
+        
+    def get_hover(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            self.is_hover = True
+        else:
+            self.is_hover = False
+
+    def change_color(self):
+        if self.is_hover:
+          self.color = hover
+        else:
+          self.color = black    
+
+      
 
     def draw(self, screen):
+
+        self.get_hover()
+       
         if self.text == "back":
             self.rect.x = 100
             self.rect.y = 100
-
-
+        
+        pygame.draw.rect(screen, self.color, self.rect)
         screen.blit(self.text_surface, (self.rect.x, self.rect.y))
